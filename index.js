e4b86c7e6385a17c8ecdeb468fe3e990625bba16
Snok <chocowaco152@gmail.noreply.github.com>
2024-11-25 15:50:15 +0800

    Create function that add books to html

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const myLibrary = [];
 
 function Book(title, author) {
-  this.title = title;
-  this.author = author;
+	this.title = title;
+	this.author = author;
 }
 
 function addBookToLibrary(title, author) {
-  const newBook = new Book(title, author);
-  myLibrary.push(newBook);
+	const newBook = new Book(title, author);
+	myLibrary.push(newBook);
+}
+
+function addBooksToTable(library) {
+	const table = document.querySelector(".book-table");
+	for (let i = 0; i < library.length; i++) {
+		const tableRow = document.createElement("tr");
+		const title = document.createElement("td");
+		const author = document.createElement("td");
+		title.textContent = library[i].title;
+		title.className = "title";
+		author.textContent = library[i].author;
+		author.className = "author";
+		tableRow.appendChild(title);
+		tableRow.appendChild(author);
+		table.appendChild(tableRow);
+	}
 }
 
 addBookToLibrary("Harry Potter", "J.K. Rolling");
 console.log(myLibrary);
+addBooksToTable(myLibrary);
