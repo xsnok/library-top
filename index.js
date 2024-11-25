ad7c8d94c220c2f060ca601c65e265fdd40d5ca2
Snok <chocowaco152@gmail.noreply.github.com>
2024-11-25 16:11:08 +0800

    Add ability to add books to table

--- index.js
+++ index.js
@@ -5,27 +5,41 @@ function Book(title, author) {
 	this.author = author;
 }
 
-function addBookToLibrary(title, author) {
+function addBookToLibrary(library, title, author) {
 	const newBook = new Book(title, author);
-	myLibrary.push(newBook);
+	library.push(newBook);
 }
 
-function addBooksToTable(library) {
+function addBookToTable(book) {
 	const table = document.querySelector(".book-table");
-	for (let i = 0; i < library.length; i++) {
-		const tableRow = document.createElement("tr");
-		const title = document.createElement("td");
-		const author = document.createElement("td");
-		title.textContent = library[i].title;
-		title.className = "title";
-		author.textContent = library[i].author;
-		author.className = "author";
-		tableRow.appendChild(title);
-		tableRow.appendChild(author);
-		table.appendChild(tableRow);
-	}
+	const tableRow = document.createElement("tr");
+	const title = document.createElement("td");
+	const author = document.createElement("td");
+	title.textContent = book.title;
+	title.className = "title";
+	author.textContent = book.author;
+	author.className = "author";
+	tableRow.appendChild(title);
+	tableRow.appendChild(author);
+	table.appendChild(tableRow);
 }
 
-addBookToLibrary("Harry Potter", "J.K. Rolling");
+function newBookEventListener() {
+	const addBookButton = document.querySelector(".add-book-button");
+	const titleInput = document.querySelector(".title-input");
+	const authorInput = document.querySelector(".author-input");
+
+	addBookButton.addEventListener("click", function() {
+		addBookToLibrary(myLibrary, titleInput.value, authorInput.value);
+		console.log(titleInput.value)
+		console.log(authorInput.value)
+		addBookToTable(myLibrary[myLibrary.length - 1]);
+	});
+}
+
+addBookToLibrary(myLibrary, "Harry Potter", "J.K. Rolling");
 console.log(myLibrary);
-addBooksToTable(myLibrary);
+for(let i = 0; i < myLibrary.length; i++) {
+	addBookToTable(myLibrary[i]);
+}
+newBookEventListener();
